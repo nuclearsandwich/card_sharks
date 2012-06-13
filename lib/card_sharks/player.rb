a500581ce4a300190e1eeb92bef2cf3f424ab70f
Anthony Severini <FluffyCode@gmail.com>
2012-06-13 14:15:42 -0700

    Add credits-method, and make_bid-method

--- lib/card_sharks/player.rb
+++ lib/card_sharks/player.rb
@@ -4,13 +4,21 @@ class Player
     @hand = []
   end
 
+  def credits
+    @credits
+  end
+
   def make_bid
     if @credits == 0
       puts "You are out of credits."
       exit 0
     end
     puts "How many of your #{@credits} credits would you like to wager?"
-    bid = gets.chomp.to_i if (bid > 0 && bid <= @credits) else make_bid
+    bid = gets.chomp.to_i
+    if (bid > 0 && bid <= @credits)
+    else 
+      make_bid
+    end
   end
 
   def deal(card)
