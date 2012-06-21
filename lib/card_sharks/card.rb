cede0c804f0382efebc056ecf871c65fae22d6fe
Anthony Severini <FluffyCode@gmail.com>
2012-06-20 19:15:04 -0700

    Rework to include card values

--- lib/card_sharks/card.rb
+++ lib/card_sharks/card.rb
@@ -1,4 +1,5 @@
-class Card < Struct.new(:suit, :rank)
+class Card < Struct.new(:suit, :rank, :value)
+  VALUES = %w[2 3 4 5 6 7 8 9 10 10 10 10 0]
   RANKS = %w[Two Three Four Five Six Seven Eight Nine Ten Jack Queen King Ace]
   SUITS = %w[Clubs Diamonds Hearts Spades]
 
@@ -17,4 +18,20 @@ class Card < Struct.new(:suit, :rank)
   def self.suits
     SUITS
   end
+
+  def self.values
+    VALUES
+  end
 end
+
+# SIZE = 52
+# deck = []
+
+# SIZE.times do |i|
+# rank = Card.ranks[i % 13]
+# value = Card.values[i % 13]
+# suit = Card.suits[i % 4]
+# deck << Card.new(suit, rank, value)
+# end
+
+# puts deck[12].value
