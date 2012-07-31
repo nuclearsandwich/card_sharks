8cf400549319fe96722360355942abc5395fdb74
Anthony Severini <FluffyCode@gmail.com>
2012-07-31 16:54:32 -0700

    Update progress

--- lib/card_sharks/go_fish.rb
+++ lib/card_sharks/go_fish.rb
@@ -18,27 +18,37 @@ require "./Dealer"
 class GoFish
 	def initialize
 		@player = Player.new
-		@opponent = Dealer.new
+		@dealer = Dealer.new
 	end
 
 	def round_of_go_fish
 		@player.wipe_hand
-		@opponent.wipe_hand
+		@dealer.wipe_hand
 
 		@deck = Deck.new
 		@deck.shuffle!
 
 		# Initial deal; 7 cards go to each player.
 		7.times { @player.deal(@deck.remove_top_card) }
-		7.times { @opponent.deal(@deck.remove_top_card) }
+		7.times { @dealer.deal(@deck.remove_top_card) }
 
 		# Ultimately, these two lines will be removed. Keep for now, while testing
 		puts "Player hand: #{@player.tell_hand}."
-		puts "Opponent hand: #{@opponent.tell_hand}."
+		puts "Opponent hand: #{@dealer.tell_hand}."
 
 		def dealers_turn
+			# dealer gets a pool of ranks to chose from:
 			cards_to_chose_from = []
-			
+			# populate the choice-pool:
+			@dealer.hand.each do |card|
+				cards_to_chose_from << card.ranks
+			end
+
+			# randomly determine which card the dealer will ask for:
+			random_card = rand(cards_to_chose_from.length)
+
+			# ask for it:
+			puts "The dealer asks for your #{random_card.ranks}s."
 		end
 
 		def go_fish(card)
@@ -47,41 +57,50 @@ class GoFish
 
 			# The player gets another turn if they got what they asked for:
 			if card_to_deal.include?("card")
-				do_you_have_any(gets.chomp)
+				puts "You got what you asked for! You get another turn."
+				ask_for
 			else
 				dealers_turn
 			end
 		end
 
 		def do_you_have_any(requested_card)
-			@opponent.hand.each do |card|
+			@dealer.hand.each do |card|
 				if card.include?(requested_card)
 					take_me = card
-					@player.deal(@opponent.hand.delete(take_me))
+					@player.deal(@dealer.hand.delete(take_me))
 					puts "The dealer had a #{requested_card}; you add the #{take_me} to your hand."
-					do_you_have_any(gets.chomp)
+					ask_for
 				else
 					# The player is told to go fish, if any cards remain in the deck:
 					go_fish(requested_card) if @deck.remove_top_card != nil
 					# Else, go to dealer's turn:
 					dealers_turn
+				end
 			end
+		end
+
+		def ask_for
+			"What rank do you want to ask your opponent for?"
+			requested_card = gets.chomp
+			do_you_have_any(requested_card)
 		end
 
 		# determine who goes first
 		def who_goes_first
-			x = rand(2)
-
-			if x == 1
-				do_you_have_any(gets.chomp)
+			# In the final version, uncomment the following line. Just for testing, player gets the first turn.
+			# if rand(2) == 1
+			if 1 == 1	
+				puts "You get the first turn."
+				ask_for
 			else
-				# opponent goes first
+				puts "The dealer gets the first turn."
+				dealers_turn
 			end
 		end
+
+		who_goes_first
 	end
-
-	who_goes_first
-
 end
 
 def play_a_game
