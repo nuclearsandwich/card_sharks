56404210cf8a57a9234ce6e74488ae2c8cd18624
Anthony Severini <FluffyCode@gmail.com>
2012-07-31 14:16:27 -0700

    Update progress with go_fish.rb

--- lib/card_sharks/go_fish.rb
+++ lib/card_sharks/go_fish.rb
@@ -21,34 +21,77 @@ class GoFish
 		@opponent = Dealer.new
 	end
 
-	@player.wipe_hand
-	@opponent.wipe_hand
+	def round_of_go_fish
+		@player.wipe_hand
+		@opponent.wipe_hand
 
-	@deck = Deck.new
-	@deck.shuffle!
+		@deck = Deck.new
+		@deck.shuffle!
 
-	# Initial deal; 7 cards go to each player.
-	7.times { @player.deal(@deck.remove_top_card) }
-	7.times { @opponent.deal(@deck.remove_top_card) }
+		# Initial deal; 7 cards go to each player.
+		7.times { @player.deal(@deck.remove_top_card) }
+		7.times { @opponent.deal(@deck.remove_top_card) }
 
-	def do_you_have_any(requested_card)
-		@opponent.hand.each do |card|
-			if card.include?(requested_card)
-				take_me = card
-				@player.deal(@opponent.hand.delete(take_me))
+		# Ultimately, these two lines will be removed. Keep for now, while testing
+		puts "Player hand: #{@player.tell_hand}."
+		puts "Opponent hand: #{@opponent.tell_hand}."
+
+		def dealers_turn
+			cards_to_chose_from = []
+			
+		end
+
+		def go_fish(card)
+			card_to_deal = @deck.remove_top_card
+			@player.deal(card_to_deal)
+
+			# The player gets another turn if they got what they asked for:
+			if card_to_deal.include?("card")
+				do_you_have_any(gets.chomp)
+			else
+				dealers_turn
+			end
+		end
+
+		def do_you_have_any(requested_card)
+			@opponent.hand.each do |card|
+				if card.include?(requested_card)
+					take_me = card
+					@player.deal(@opponent.hand.delete(take_me))
+					puts "The dealer had a #{requested_card}; you add the #{take_me} to your hand."
+					do_you_have_any(gets.chomp)
+				else
+					# The player is told to go fish, if any cards remain in the deck:
+					go_fish(requested_card) if @deck.remove_top_card != nil
+					# Else, go to dealer's turn:
+					dealers_turn
+			end
+		end
+
+		# determine who goes first
+		def who_goes_first
+			x = rand(2)
+
+			if x == 1
+				do_you_have_any(gets.chomp)
+			else
+				# opponent goes first
 			end
 		end
 	end
 
-	# determine who goes first
-	def go_first
-		x = rand(2)
-
-		if x == 1
-			do_you_have_any(gets.chomp)
-		else
-			# opponent goes first
-		end
-	end
+	who_goes_first
 
 end
+
+def play_a_game
+	puts "Would you like to play a game of Go Fish?"
+	if gets.chomp.downcase == "yes"
+		GoFish.new.round_of_go_fish
+	else 
+		puts "Alrighty then, another time!"
+		exit 0
+	end
+end
+
+play_a_game
